d8f42552229e9ef856cd7a486b58161d3abfd666
Евгений Балабат <ybalabat@MacBook-Air-Evgenij.local>
2023-04-24 20:02:51 +0200

    finaly update

--- dist/hw3_task1.js
+++ dist/hw3_task1.js
@@ -1,46 +1,59 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const funcs = [
     () => new Promise((resolve) => {
         const data = { message: 'alles gut' };
+        console.log(new Date());
         resolve(data);
     }),
     () => new Promise((resolve) => {
         const data = { message: 'alles gut' };
+        console.log(new Date());
         resolve(data);
     }),
     () => new Promise((resolve) => {
         const data = { message: 'alles gut' };
+        console.log(new Date());
         resolve(data);
     }),
     () => new Promise((resolve) => {
         const data = { message: 'alles gut' };
+        console.log(new Date());
         resolve(data);
     }),
     () => new Promise((resolve) => {
         const data = { message: 'alles gut' };
+        console.log(new Date());
         resolve(data);
     }),
     () => new Promise((resolve) => {
         const data = { message: 'alles gut' };
+        console.log(new Date());
         resolve(data);
     }),
 ];
 function mainFunc(funcsPromises, tasksPerSec, seconds) {
-    const resultArr = [];
-    const arrPromises = [];
-    funcsPromises.forEach((func) => {
-        arrPromises.push(func());
-    });
-    const interval = setInterval(() => {
-        Promise.all(arrPromises.splice(0, tasksPerSec)).then((results) => {
-            results.forEach((element) => {
-                resultArr.push(element);
-                console.log(`${element.message} ${new Date()}`);
-            });
-            if (!arrPromises.length)
-                clearInterval(interval);
+    return __awaiter(this, void 0, void 0, function* () {
+        return new Promise((resolve) => {
+            const resultG = [];
+            const interval = setInterval(() => __awaiter(this, void 0, void 0, function* () {
+                const resultL = yield Promise.all(funcsPromises.splice(0, tasksPerSec).map((func) => func()));
+                resultG.push(...resultL);
+                if (!funcsPromises.length) {
+                    clearInterval(interval);
+                    resolve(resultG);
+                }
+            }), seconds * 1000);
         });
-    }, seconds * 1000);
+    });
 }
-mainFunc(funcs, 2, 5);
+mainFunc(funcs, 2, 1).then(console.log);
--- src/hw3_task1.ts
+++ src/hw3_task1.ts
@@ -1,60 +1,64 @@
+import { resolve } from 'path'
 import { ResolvedData } from './interface'
 
 const funcs: Array<Function> = [
     (): Promise<ResolvedData> =>
         new Promise((resolve) => {
             const data: ResolvedData = { message: 'alles gut' }
+            console.log(new Date())
             resolve(data)
         }),
     (): Promise<ResolvedData> =>
         new Promise((resolve) => {
             const data: ResolvedData = { message: 'alles gut' }
+            console.log(new Date())
             resolve(data)
         }),
     (): Promise<ResolvedData> =>
         new Promise((resolve) => {
             const data: ResolvedData = { message: 'alles gut' }
+            console.log(new Date())
             resolve(data)
         }),
     (): Promise<ResolvedData> =>
         new Promise((resolve) => {
             const data: ResolvedData = { message: 'alles gut' }
+            console.log(new Date())
             resolve(data)
         }),
     (): Promise<ResolvedData> =>
         new Promise((resolve) => {
             const data: ResolvedData = { message: 'alles gut' }
+            console.log(new Date())
             resolve(data)
         }),
     (): Promise<ResolvedData> =>
         new Promise((resolve) => {
             const data: ResolvedData = { message: 'alles gut' }
+            console.log(new Date())
             resolve(data)
         }),
 ]
 
-function mainFunc(
+async function mainFunc(
     funcsPromises: Array<Function>,
     tasksPerSec: number,
     seconds: number
-): void {
-    const resultArr: Array<ResolvedData> = []
-    const arrPromises: Array<Promise<ResolvedData>> = []
+): Promise<Array<ResolvedData>> {
+    return new Promise((resolve) => {
+        const resultG: Array<ResolvedData> = []
+        const interval = setInterval(async () => {
+            const resultL = await Promise.all(
+                funcsPromises.splice(0, tasksPerSec).map((func) => func())
+            )
+            resultG.push(...resultL)
 
-    funcsPromises.forEach((func) => {
-        arrPromises.push(func())
+            if (!funcsPromises.length) {
+                clearInterval(interval)
+                resolve(resultG)
+            }
+        }, seconds * 1000)
     })
-
-    const interval = setInterval(() => {
-        Promise.all(arrPromises.splice(0, tasksPerSec)).then((results) => {
-            results.forEach((element) => {
-                resultArr.push(element)
-                console.log(`${element.message} ${new Date()}`)
-            })
-
-            if (!arrPromises.length) clearInterval(interval)
-        })
-    }, seconds * 1000)
 }
 
-mainFunc(funcs, 2, 5)
+mainFunc(funcs, 2, 1).then(console.log)
